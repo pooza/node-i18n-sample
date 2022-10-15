f1b43c1250d239931ae9fa9093c51f3d410fc4ea
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-10-15 20:58:12 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world',
+    },
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界。',
+    },
+  },
+}
+
+const i18n = createI18n({
+  locale: 'ja',
+  messages,
+})
+
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
